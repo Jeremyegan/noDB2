df03fafac5a7f14907f5b0bd584672fae705c2e6
Jeremyegan <jeremyegan1@gmail.com>
2019-05-28 16:22:36 -0600

    Finished

---
+++ facebook-html-toy/src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="menu">
+          <div className="button">
+          </div>
+          <div className="button2">
+            <div className="one"></div>
+            <div className="two"></div>
+          </div>
+        </div>
+        <div className="top-right-box">
+        </div>
+      </header>
+
+      <div className="main-img"></div>
+      <div className="bottom-container">
+        <div classname="one1">
+          <div className="one2"></div>
+          <div className="two1"></div>
+        </div>
+        <div className="two2">
+          <div className="three1"></div>
+          <div className="three2"></div>
+        </div>
+      </div>
+
+      <div className="footer1">
+        <div className="dose">
+          <div className="line1"></div> 
+          <div className="line2"></div>
+        </div>
+        <div className="trace">
+          <div className="btn-1"></div>
+          <div className="btn-2"></div>
+        </div>
+      </div>
+
+      <footer>
+        <div className="btm-menu">
+          <div className="btn-1"></div>
+          <div className="btn-2"></div>
+        </div>
+        <div className="btm-menu1">
+          <div className="btn-1"></div>
+          <div className="btn-2"></div>
+        </div>
+        <div className="btm-menu1">
+          <div className="btn-1"></div>
+          <div className="btn-2"></div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
